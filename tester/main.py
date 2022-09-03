0cbe5a1057cba90ff6bdbb662475f51e6c1d6f5c
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-09-03 11:26:10 -0400

    Remove 503 response code

--- tester/main.py
+++ tester/main.py
@@ -1,7 +1,7 @@
 import os
 
 from deta import App
-from fastapi import FastAPI, HTTPException, Response, status
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 from requests_futures.sessions import FuturesSession
 from tests import BaseTests, DriveTests, MicroTests
@@ -20,13 +20,10 @@ async def root():
 
 
 @app.get('/{test}')
-async def test(test: str, response: Response):
+async def test(test: str):
     if test not in tests.keys():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    results = tests[test]().run()
-    if not results['passed']:
-        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
-    return results
+        raise HTTPException(status_code=404)
+    return tests[test]().run()
 
 
 @app.lib.cron()
