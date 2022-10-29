787c3e8e5ceaaa510246d932803c49531f27bb13
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-10-28 23:03:02 -0400

    Remove unused deta dependency

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,4 +1,3 @@
-from deta import Deta
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -7,7 +6,6 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
-deta = Deta()
 
 
 @app.get('/', response_class=HTMLResponse)
